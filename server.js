f4ec0bf501ab1b64747612ae0254f27224c24acc
diamondcattoon-cmd <diamondcattoon@gmail.com>
2026-06-11 15:15:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(__dirname));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
 
